26983a0e2df1cdfd38ca1bbe003964835239707c
BMatlosz <matlosz.bartlomiej@gmail.com>
2019-01-25 12:21:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 
 function function_scaneDev() {
     console.log("Click scane button");
-    navigator.bluetooth.requestDevice({
-        acceptAllDevices: true
-    })
-        .then(device => { /* ... */ })
+    navigator.bluetooth.requestDevice({services: ["Puck.js", "Pixl.js"]})
+        .then(device => { device.name })
         .catch(error => { console.log(error); });
 
 }
