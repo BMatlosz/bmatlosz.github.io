c1365a9e45036ff3c134c4866675dcfc68b32be4
BMatlosz <matlosz.bartlomiej@gmail.com>
2019-01-25 12:29:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 
+var NORDIC_SERVICE = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+
 function function_scaneDev() {
     console.log("Click scane button");
     navigator.bluetooth.requestDevice({
         filters: [
             { namePrefix: 'Puck.js' },
-            { namePrefix: 'Pixl.js' }
+            { namePrefix: 'Pixl.js' },
+            { services: [NORDIC_SERVICE] }
         ]
     })
-        .then(device => { device.name })
+        .then(function (device) {
+            console.log(1, 'Device Name:       ' + device.name);
+            console.log(1, 'Device ID:         ' + device.id);
+        })
         .catch(error => { console.log(error); });
 
 }
