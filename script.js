a863f96b283c3e0c961abc4c02757c1c33f4fadf
BMatlosz <matlosz.bartlomiej@gmail.com>
2019-02-07 15:12:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 ID BLE Devices
 */
 
-document.body.requestFullscreen();
-
-
 var NORDIC_SERVICE = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 var NORDIC_TX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 var NORDIC_RX = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
