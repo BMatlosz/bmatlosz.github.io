4fc24d98ef4de43e987accce8935ff69e80b7ffb
BMatlosz <matlosz.bartlomiej@gmail.com>
2019-02-15 10:02:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ ID BLE Devices
 */
 
 var NORDIC_SERVICE = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
-var STM32_SERVICE =  '0100fe40-cc7a-482a-984a-7f2ed5b3e58f';
+var STM32_SERVICE =  '1100fe40-cc7a-482a-984a-7f2ed5b3e58f';
 var NORDIC_TX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 var NORDIC_RX = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
